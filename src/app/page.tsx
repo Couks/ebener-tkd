fa10655f6b622b46928e6a5bfd482bac4042ccb8
Matheus Castro <matheus.castroks@gmail.com>
2024-07-18 20:52:22 -0300

    add build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 import quemSomos from "@/assets/images/image8.jpeg";
 import { Button } from "@/components/ui/button";
