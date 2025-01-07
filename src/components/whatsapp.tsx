b50503d777badb644c874d9153df9cdde46376d4
Matheus Castro <49662686+Couks@users.noreply.github.com>
2025-01-07 20:24:02 -0300

    another whatsapp button adjust

--- src/components/whatsapp.tsx
+++ src/components/whatsapp.tsx
@@ -9,7 +9,7 @@ const WhatsappButton: React.FC = () => {
       href="https://wa.me/5521981654811"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-4 right-2 z-50 animate-bounce"
+      className="fixed bottom-4 right-3 z-50 animate-bounce"
     >
       <Image src={whatsapp} alt="WhatsApp" className="size-12 rounded-full" />
     </a>
