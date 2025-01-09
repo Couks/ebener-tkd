39391d7e5ff0cac563936c5b674fa6aa87010684
Matheus Castro <49662686+Couks@users.noreply.github.com>
2025-01-09 03:54:36 -0300

    add custom 404 page

---
+++ src/app/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Custom404() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-center text-gray-800 mb-4">
+        404 - Página Não Encontrada
+      </h1>
+      <p className="text-lg text-gray-600 text-center">
+        A página que você está procurando não foi encontrada.
+      </p>
+    </div>
+  );
+}
