7d49ca9033c621e9e380120c40960dde17f1d616
Matheus Castro <matheuscastroks@gmail.com>
2025-05-20 20:03:52 -0300

    add metadata noindex for recibos page

--- src/app/(pages)/recibos/page.tsx
+++ src/app/(pages)/recibos/page.tsx
@@ -1,5 +1,17 @@
 import ReciboForm from "@/components/recibos/recibo-form";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    noimageindex: true,
+    "max-snippet": -1,
+    "max-image-preview": "none",
+    "max-video-preview": -1,
+  },
+};
 
 export default function Recibos() {
   return (
